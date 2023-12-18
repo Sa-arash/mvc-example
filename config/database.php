f70861380c10f4b2bf88a46d6270444b4bc61032
saman <138027864+Sa-arash@users.noreply.github.com>
2023-12-18 16:45:25 +0330

    Created DBconfig

---
+++ config/database.php
@@ -0,0 +1,16 @@
+<?php 
+return [
+
+    'HOST' => 'localhost',
+
+    'DB_USER' => 'root',
+
+    'DB_PASS' => '',
+
+    'DB_NAME' => 'mvc',
+
+    'DB_DRIVER' => 'mysql',
+
+    'CHARSET' => 'utf-8'
+
+];
